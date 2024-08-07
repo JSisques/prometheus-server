02abb63f6290e7597b708f6f1cf921185a917c59
JSisques <javierplazasisques@gmail.com>
2024-08-07 21:48:04 +0200

    v1.0.0

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,18 +6,18 @@ SOURCE_DIR="$USER_DIR/data"
 BACKUP_DIR="$USER_DIR/backups"
 TIMESTAMP=$(date +"%Y-%m-%d")
 BACKUP_FILE="$TIMESTAMP.tar.gz"
-CONFIG_FILES=(
-    "$USER_DIR/config/prometheus.yml"
-    "$USER_DIR/config/rules.yml"
-    "$USER_DIR/config/alertmanager.yml"
-    "$USER_DIR/.env"
-)
+
+# Definir las rutas a los archivos de configuración como variables separadas
+CONFIG_PROMETHEUS="$USER_DIR/config/prometheus.yml"
+CONFIG_RULES="$USER_DIR/config/rules.yml"
+CONFIG_ALERTMANAGER="$USER_DIR/config/alertmanager.yml"
+CONFIG_ENV="$USER_DIR/.env"
 
 # Crear directorio de respaldo si no existe
 mkdir -p "$BACKUP_DIR"
 
 # Copiar archivos de configuración al directorio de respaldo si existen
-for file in "${CONFIG_FILES[@]}"; do
+for file in "$CONFIG_PROMETHEUS" "$CONFIG_RULES" "$CONFIG_ALERTMANAGER" "$CONFIG_ENV"; do
     if [ -f "$file" ]; then
         echo "Copiando archivo de configuración: $file"
         cp "$file" "$BACKUP_DIR/"
@@ -28,7 +28,7 @@ done
 
 # Realizar la copia de seguridad de la carpeta principal
 echo "Realizando la copia de seguridad de $SOURCE_DIR..."
-tar -zcvf "$BACKUP_DIR/$BACKUP_FILE" -C "$BACKUP_DIR" .
+tar -zcvf "$BACKUP_DIR/$BACKUP_FILE" -C "$SOURCE_DIR" .
 
 # Imprimir mensaje de éxito
 echo "Copia de seguridad realizada con éxito: $BACKUP_DIR/$BACKUP_FILE"
